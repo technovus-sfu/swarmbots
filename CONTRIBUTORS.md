e26a34f3468fe9709c782bf47926e2f297486783
Muhammad Abid <abidsaleem682@gmail.com>
2018-02-05 20:44:54 -0800

    Added my tests

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -1,3 +1,4 @@
 # Contributors :star: :crown:
 ## 2018
 - [Steven Richter](https://github.com/stevenrichter)
+- Muhammad Abid
---
+++ software/test/sample.py
@@ -0,0 +1,14 @@
+# print("Hello World!")
+# sum = 2 + 2
+# print(sum)
+
+# for i in range(10,-10,-1):
+#     if i % 2 == 0:
+#         print(i)
+#     else:
+#         pass
+
+# while(1):
+#     val = input("Enter ")
+#     print(val)
+
