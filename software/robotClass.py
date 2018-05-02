0ab7f1a315fb15ffb2af5b85e532a2ac95424c2d
Harkirat Gill <hsg12@sfu.ca>
2018-05-01 20:32:24 -0700

    Added Harkirat's Files for Vision and intelligence

---
+++ software/robotClass.py
@@ -0,0 +1,103 @@
+import serial
+import string
+import math
+
+class robot:
+
+	address = "/dev/cu.HC-05-DevB"
+	speed = 0;
+	current_position = [0,0,0]
+	target_position = [0, 0]
+	distance = 0;
+	angle_diff = 0;
+	compliment = 0;
+
+	def __init__ (self, address, target):
+		self.address = address
+		self.target_position = target
+	#
+	port = serial.Serial(address, 9600)
+
+	# method to move the robot
+	def move(self):
+		self.calc_dist_angle()
+
+		print "angle ", self.angle_diff, "distance", self.distance, "compliment", self.compliment
+
+		if 10 <= abs(self.compliment) <= 80 and self.distance > 60:
+			print "orientating"
+			self.orient()
+
+		elif self.distance > 70:
+			print "moving"
+			if 160 <= abs(self.angle_diff) <= 200:
+				print "should go forward"
+				self.forward()
+			elif math.floor(abs(self.angle_diff)) in range (0,20)+range(340,360):
+				print "should go backward"
+				self.backward()
+		#
+		else:
+			self.stop();
+
+	# method to move the robot forward
+	def forward(self):
+		if abs(self.speed) == 0.5:
+			self.speed = 0
+		#
+		ratio = int(math.ceil((self.distance*8)/1000))
+		if self.speed < 2:
+			# for i in range(0,ratio):
+			print "forward ", ratio, self.speed
+			self.port.write("w")
+			self.speed = self.speed+1;
+
+	# method to move the robot backward
+	def backward(self):
+		ratio = int(math.ceil((self.distance*8)/1000))
+		if self.speed > -2:
+			print "backward", ratio, self.speed
+			# for i in range(0,ratio):
+			self.port.write("s")
+			self.speed = self.speed-1;
+
+	# method to stop the robot
+	def stop(self):
+		print "stopped"
+		self.port.write("q")
+		self.speed = 0
+
+	# method to find the required orientation
+	def orient(self):
+		if abs(self.speed) > 0.5:
+			self.speed = 0
+		#
+		# left_turn_conditions = range(-45,0)+range(135,180)+range(-225,-180)+range(315,360)
+		# right_turn_conditions = range(0,45)+range(-180,-135)+range(180, 225)+range(-360,-315)
+		left_turn_conditions = range(-90,0)+range(90,180)+range(-270,-180)+range(270,360)
+		right_turn_conditions = range(0,90)+range(-180,-90)+range(180, 270)+range(-360,-270)
+		if math.floor(self.angle_diff) in left_turn_conditions and (self.speed > -0.5):
+			print "left"
+			self.port.write("a")
+			self.speed = self.speed - 0.5
+		elif math.floor(self.angle_diff) in right_turn_conditions and (self.speed < 0.5):
+			print "right"
+			self.port.write("d")
+			self.speed = self.speed + 0.5
+
+	# method to calculate the distance and orientation difference
+	def calc_dist_angle(self):
+		x_delta = self.target_position[0] - self.current_position[0]
+		y_delta = self.target_position[1] - self.current_position[1]
+		self.distance = math.hypot(x_delta, y_delta)
+
+		required_orientation = math.atan2(y_delta, x_delta) * 180/math.pi 
+		current_orientation = self.current_position[2]
+		
+		self.angle_diff = (required_orientation - current_orientation)
+
+		#calculates the compliment of angle [0, 90] in each quadrant
+		self.compliment = abs(self.angle_diff) - math.floor( abs(self.angle_diff)/90 )*90
+
+		# print required_orientation, current_orientation, self.angle_diff, self.compliment
+
---
+++ software/vision.py
@@ -0,0 +1,70 @@
+import numpy as np
+import cv2
+import math
+
+# finds coordinates of the circle
+def find_location(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    minR = 50;
+    maxR = 125;
+    # # # # # cv2.HoughCircles(image, method, dp, minDist)
+    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT, 2, (2*.9)*minR,
+        param1=150, param2=100, minRadius=minR, maxRadius=maxR) #150/100
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        # returns circles = [x_Centre y_centre radius]
+        return circles
+
+# find contours of color specified
+def rangeContours(hsv, colorLower, colorUpper):
+    mask = cv2.inRange(hsv, colorLower, colorUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    # returns contours of the color speicified
+    return cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# finds coordinates of the green orientation block
+def find_orientation_block(frame):
+    sensitivity = 50
+    #bgr
+    greenLower = (90, 150, 50)
+    greenUpper = (186, 255, 130)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    cont_frame, contours, hierarchy = rangeContours(frame,greenLower, greenUpper)
+    centers = []
+    
+    for i, cont in enumerate(contours, start=0):
+        M = cv2.moments(cont)
+        centers.append( [int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])] )
+    # returns centres of green block + contours to plot it
+    return [centers, contours]
+
+# returns the location (of centre) + orientation of the robot
+def find_robots(frame):
+    circles                         = find_location(frame);
+    (green_centre, green_contours)  = find_orientation_block(frame);
+    robots = []
+    if circles is not None:
+        for i in circles[0,:]:
+            cv2.circle(frame, (i[0],i[1]),i[2],(255,0,0),2); # draw the outer circle
+            cv2.circle(frame, (i[0],i[1]),2,   (0,0,255),3); # draw the center of the circle
+            if len(green_centre) > 0:
+                cv2.drawContours(frame, green_contours, -1, (0,255,0), 1) #draw green contours
+                for j in green_centre:
+                    cv2.circle(frame,(j[0],j[1]),2,(0,0,255),3); # draw the center of the green
+                    x_delta = i[0] - j[0];
+                    y_delta = i[1] - j[1];
+                    distance = math.hypot(x_delta, y_delta);
+                    angle = math.atan2(y_delta, x_delta) * 180/math.pi;
+                    # only mates the green blocks to close by circle
+                    if (distance < i[2]*1.5): 
+                        robots.append([i[0], i[1], angle]);
+
+    # returns stats on robots [x_pos; y_pos; angle]
+    return robots
+
+
