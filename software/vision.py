a7973f996cac049b00bb8a3ab19dca1a1617030c
belithioben <benjaminatbi@gmail.com>
2018-05-11 19:19:05 -0700

    added match function

--- software/vision.py
+++ software/vision.py
@@ -68,3 +68,10 @@ def find_robots(frame):
     return robots
 
 
+def matchRobots(oldPositions, newPositions):
+    for newpos in newPositions:
+        for i in range(len(oldPositions)):
+            if math.hypot(newpos[0] - oldPositions[i][0], newpos[1] - oldPositions[i][1]) < 50 and abs(newpos[2] - oldPositions[i][2]) < 20:
+                oldPositions[i] = newpos
+                break
+    return oldPositions
